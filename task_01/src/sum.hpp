eac26439d10fe744f9fa14481ccea9d2c9359043
Ivan Khokhlov <lostpointmeister@gmail.com>
2024-09-14 08:30:27 +0000

    fix format

--- task_01/src/sum.hpp
+++ task_01/src/sum.hpp
@@ -1,1 +1,3 @@
-int Sum(int first_number, int second_number, int third_number) { return first_number + second_number + third_number; }
+int Sum(int first_number, int second_number, int third_number) {
+  return first_number + second_number + third_number;
+}
