cc62a2493731bb41742e1b1f1c1def91fb97034d
Ivan Khokhlov <lostpointmeister@gmail.com>
2024-09-14 08:28:28 +0000

    do homework

--- task_01/src/sum.hpp
+++ task_01/src/sum.hpp
@@ -1,1 +1,1 @@
-int Sum(int first_number, int second_number, int third_number) { return 6; }
+int Sum(int first_number, int second_number, int third_number) { return first_number + second_number + third_number; }
--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -2,4 +2,7 @@
 
 #include <sum.hpp>
 
-TEST(Test, Simple) { ASSERT_EQ(Sum(1, 2, 3), 6); }
+TEST(Test, Simple) {
+  ASSERT_EQ(Sum(1, 2, 3), 6);
+  ASSERT_EQ(Sum(1, 2, 5), 8);
+}
